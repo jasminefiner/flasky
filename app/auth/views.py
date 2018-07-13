74fcfb7402b7cf7467c741b90a9f2c88646cc2b2
Jasmine Finer <jasminefiner@me.com>
2018-07-13 11:51:47 +0100

    fixed bugs and migrated database

--- app/auth/views.py
+++ app/auth/views.py
@@ -8,8 +8,10 @@ from .forms import LoginForm, RegistrationForm, PasswordResetRequestForm, Passwo
 
 @auth.before_app_request
 def before_request():
-    if current_user.is_authenticated and not current_user.confirmed and request.blueprint != 'auth' and request.endpoint != 'static':
-        return redirect(url_for('auth.unconfirmed'))
+    if current_user.is_authenticated:
+        current_user.ping()
+        if not current_user.confirmed and request.endpoint and request.blueprint != 'auth' and request.endpoint != 'static':
+            return redirect(url_for('auth.unconfirmed'))
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
